8b51960566551878cab3ea1dc4897369f7c88455
artun <artunankara@gmail.com>
2023-06-22 11:11:00 +0300

    intro py

---
+++ pyBasic/pyhtonBasic.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 10:15:08 2023
+
+@author: artun
+"""
+
+#variables
+#function
+#object
+#%% section start (shift + enter)
+var1 = 10 # intager
+var2 = 15
+
+day = "monday" #string
+
+var3 = 10.5 #double
+#%% string
+
+s = "to day is monday"
+
+variable_type = type(s)
+
+print(s)
+
+var1 = "ankara"
+var2 = "ist"
+var3 = var1 + var2
+
+var4 = "100"
+var5 = "200"
+var6 = var4 + var5
+uzunluk = len(var6)
+
+## var6[3]
+
+#%% numbers
+
+intagerDeneme = 10
+floatDeneme = 20 
+#%% Build in function
+
+str1 = "artun"
+float1 = 10.5
+
+#float(10)
+#int(float1)
+#str(float1)
+
+str2 = "100"
+#%% user defined function 
+
+var1 = 10
+var2 = 100
+
+output = (((var1 + var2)*50)/100.0)*var1/var2
+
+def functionExample(var1,var2):
+    output = (((var1 + var2)*50)/100.0)*var1/var2
+    return output
+    
+print(output)
+print(functionExample(var1,var2))
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
